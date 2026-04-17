23c7a8a8f882d5428d1b7503c2b7ae62179183e8
Axkyro <miron.victor11@protonmail.com>
2026-04-17 11:24:51 +0300

    started working on implementing actual filesystem commands

---
+++ src/commands.c
@@ -0,0 +1,13 @@
+#include "commands.h"
+#include "types.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+
+
+void dummy() {
+    
+}
--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "utils.h"
 
 int main(int argc, char **argv) {
-    printf("Sizeof: %lu\n", sizeof(Report));
     Command cmd;
     printf("Result: %d\n", parse_arguments(argc, argv, &cmd));
     command_dump(&cmd);
